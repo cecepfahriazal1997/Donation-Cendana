ad2671c5cae5a99419768c3573f9508829f549e8
Cecep Rokani <cecepfahriazal1997@gmail.com>
2021-02-15 03:20:24 +0700

    Add dashboard

--- application/controllers/backend/Page.php
+++ application/controllers/backend/Page.php
@@ -4,12 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Page extends CI_Controller {
     public function __construct() {
 		parent::__construct();
+		
+		if (!$this->session->userdata('id'))
+			redirect('backend/auth');
 
 		$this->twig->addGlobal('name', $this->session->userdata('name'));
 		$this->twig->addGlobal('image', base_url() . 'assets/dist/img/default-user.png');
 	}
 	
 	public function index() {
-		$this->twig->display('frontend/index');
+		$data['title']	    = "Dashboard";
+		$this->twig->display('backend/components/dashboard', $data);
 	}
 }
