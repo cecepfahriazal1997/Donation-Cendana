5db4e28a0044d3dc30c3643b90ec2578537e2cec
Cecep Rokani <cecepfahriazal1997@gmail.com>
2021-02-16 01:52:08 +0700

    Create mockup backend

--- application/controllers/backend/Page.php
+++ application/controllers/backend/Page.php
@@ -16,4 +16,40 @@ class Page extends CI_Controller {
 		$data['title']	    = "Dashboard";
 		$this->twig->display('backend/components/dashboard', $data);
 	}
+	
+	public function cardFamily() {
+		$data['title']	    = "Data Kartu Keluarga";
+		$data['currMenu']	= 'cardFamily';
+		$this->twig->display('backend/components/card_family/list', $data);
+	}
+	
+	public function cardFamilyDetail() {
+		$data['title']	    = "Data Anggota Keluarga";
+		$data['currMenu']	= 'cardFamily';
+		$this->twig->display('backend/components/card_family/list_family', $data);
+	}
+	
+	public function formCardFamily() {
+		$data['title']	    = "Kartu Keluarga";
+		$data['currMenu']	= 'cardFamily';
+		$this->twig->display('backend/components/card_family/form', $data);
+	}
+	
+	public function formFamily() {
+		$data['title']	    = "Anggota Keluarga";
+		$data['currMenu']	= 'cardFamily';
+		$this->twig->display('backend/components/card_family/form_family', $data);
+	}
+	
+	public function donation() {
+		$data['title']	    = "Data Donasi";
+		$data['currMenu']	= 'donation';
+		$this->twig->display('backend/components/donation/list', $data);
+	}
+	
+	public function report() {
+		$data['title']	    = "Laporan";
+		$data['currMenu']	= 'report';
+		$this->twig->display('backend/components/report/list', $data);
+	}
 }
