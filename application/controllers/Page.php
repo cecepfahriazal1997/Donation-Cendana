ed5595d36e3453b9427613c35a334403ab35f81e
Cecep Rokani <cecepfahriazal1997@gmail.com>
2021-03-07 03:37:30 +0700

    Remove unused variable

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -5,9 +5,6 @@ class Page extends CI_Controller {
     public function __construct() {
 		parent::__construct();
         $this->load->model('MidtransModel', 'midtrans');
-
-		$this->twig->addGlobal('name', $this->session->userdata('name'));
-		$this->twig->addGlobal('image', base_url() . 'assets/dist/img/default-user.png');
 	}
 	
 	public function index() {
